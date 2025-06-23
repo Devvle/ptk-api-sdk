cffd05e5970f05095989ac43e4685b2ed30b3e4f
David Pinchen <davidpinchen@gmail.com>
2025-06-23 16:19:58 +0100

    Format all files

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ This is the PromptTK API SDK, which provides a convenient way to interact with t
 ## Current Version
 
 The current version of the SDK is `0.0.1`. This version includes basic functionality for making requests to the API and handling responses. Currently you can verify your API key (free) and request a prompt (paid).
-
-Full api documentation can be found at [docs.prompttk.com](https://docs.prompttk.com).
--- src/types/prompt.types.ts
+++ src/types/prompt.types.ts
@@ -1,19 +1,19 @@
 /* Copyright (c) 2025 - Devvle - PromptTK */
 
 export type PromptTemplate = {
-  endUserDescription: string;
-  promptSenderDescription: string;
-  relationship: string;
-  mustHaves: string;
-  contextRequirements: string;
-  lengthRequirements: string;
-  avoidElements: string;
-  styleAndTone: string[];
-  examplePrompts: string;
-  mainPrompt: string;
-  outputType: string;
-  responseFormat: string;
-  promptGoal: string;
-  hallucinationControl: string;
-  projectId: string;
+	endUserDescription: string;
+	promptSenderDescription: string;
+	relationship: string;
+	mustHaves: string;
+	contextRequirements: string;
+	lengthRequirements: string;
+	avoidElements: string;
+	styleAndTone: string[];
+	examplePrompts: string;
+	mainPrompt: string;
+	outputType: string;
+	responseFormat: string;
+	promptGoal: string;
+	hallucinationControl: string;
+	projectId: string;
 };
