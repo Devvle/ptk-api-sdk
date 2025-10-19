60e6786ae4f25a32de685110c4dd0ba2b3e5ffa1
David Pinchen <davidpinchen@gmail.com>
2025-10-19 08:00:08 +0100

    Refactor index.ts to reorganize project management imports and ensure proper export of functions

--- src/apiRoutes/index.ts
+++ src/apiRoutes/index.ts
@@ -1,11 +1,21 @@
 import { validateApiKey } from './keys.route';
 import {
+	createProject,
+	deleteProject,
+	getAllProjects,
+	getProjectById,
+	getProjectsWithLimit,
+	getRecentProjects,
+	updateProject
+} from './project.route';
+import {
 	deletePrompt,
 	fetchAllPrompts,
 	fetchPromptById,
 	requestPrompt,
 	updatePrompt
 } from './prompt.route';
+import { get90DaysUsage, getUsageSummary } from './usage.route';
 import {
 	activatePromptVersion,
 	createPromptVersion,
@@ -16,13 +26,22 @@ import {
 
 export {
 	activatePromptVersion,
+	createProject,
 	createPromptVersion,
+	deleteProject,
 	deletePrompt,
 	deletePromptVersion,
 	fetchAllPrompts,
 	fetchPromptById,
+	get90DaysUsage,
+	getAllProjects,
+	getProjectById,
+	getProjectsWithLimit,
+	getRecentProjects,
+	getUsageSummary,
 	listPromptVersions,
 	requestPrompt,
+	updateProject,
 	updatePrompt,
 	updatePromptVersion,
 	validateApiKey
